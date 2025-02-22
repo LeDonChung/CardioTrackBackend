67e07546db045d617cce41aaa488f1a1bc63517c
thahhtuyenn <thanhtuyen9623@gmail.com>
2025-02-22 20:43:54 +0700

    create: notification service

---
+++ NotificationService/src/main/java/vn/edu/iuh/fit/notification/controllers/MailController.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.fit.notification.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mail")
+public class MailController {
+}
