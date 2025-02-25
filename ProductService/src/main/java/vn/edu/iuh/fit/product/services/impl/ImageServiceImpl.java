cf83b3fa2debcc7dcfcc034918a599fb524ac19e
Mai Chien No <nomai6789@gmail.com>
2025-02-25 23:04:20 +0700

    Product-service: add handle image of medicine

--- ProductService/src/main/java/vn/edu/iuh/fit/product/services/impl/ImageServiceImpl.java
+++ ProductService/src/main/java/vn/edu/iuh/fit/product/services/impl/ImageServiceImpl.java
@@ -2,9 +2,15 @@ package vn.edu.iuh.fit.product.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.edu.iuh.fit.product.exceptions.MedicineImageException;
+import vn.edu.iuh.fit.product.mappers.MedicineImageMapper;
+import vn.edu.iuh.fit.product.models.dtos.responses.MedicineImageResponse;
 import vn.edu.iuh.fit.product.models.entities.MedicinesImage;
 import vn.edu.iuh.fit.product.repositories.ImageRepository;
 import vn.edu.iuh.fit.product.services.ImageService;
+import vn.edu.iuh.fit.product.utils.SystemConstraints;
+
+import java.util.Optional;
 
 @Service
 public class ImageServiceImpl implements ImageService {
@@ -12,9 +18,16 @@ public class ImageServiceImpl implements ImageService {
     @Autowired
     private ImageRepository imageRepository;
 
+    @Autowired
+    private MedicineImageMapper medicineImageMapper;
+
     //Get by id
     @Override
-    public MedicinesImage getImageById(Long id) {
-        return imageRepository.findById(id).orElse(null);
+    public MedicineImageResponse getImageById(Long id) throws MedicineImageException {
+        Optional<MedicinesImage> medicinesImage = imageRepository.findById(id);
+        if(medicinesImage.isEmpty()) {
+            throw new MedicineImageException(SystemConstraints.MEDICINE_IMAGE_NOT_FOUND);
+        }
+        return medicineImageMapper.toResponse(medicinesImage.get());
     }
 }
