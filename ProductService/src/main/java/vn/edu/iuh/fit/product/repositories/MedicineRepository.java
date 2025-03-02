d81d5c5a102ec6e72b07567d0dc970d6167fce93
thahhtuyenn <thanhtuyen9623@gmail.com>
2025-03-02 23:20:04 +0700

    update: api product filter

--- ProductService/src/main/java/vn/edu/iuh/fit/product/repositories/MedicineRepository.java
+++ ProductService/src/main/java/vn/edu/iuh/fit/product/repositories/MedicineRepository.java
@@ -3,17 +3,19 @@ package vn.edu.iuh.fit.product.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.product.models.entities.Medicine;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Set;
 
 @Repository
-public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+public interface MedicineRepository extends JpaRepository<Medicine, Long>, JpaSpecificationExecutor<Medicine> {
 
     @Query("SELECT COUNT(distinct(m.id)) FROM Medicine m JOIN m.categories c WHERE c.id = ?1")
     int countProductByCategory(Long id);
