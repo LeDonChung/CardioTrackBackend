d7ba678e52da7e2cc5669878ceb6eede3b1e6c4b
leDonChung <ledonchung12a2@gmail.com>
2025-03-03 18:59:48 +0700

    change config gateway

--- UserService/src/main/java/vn/edu/iuh/fit/user/controllers/AddressController.java
+++ UserService/src/main/java/vn/edu/iuh/fit/user/controllers/AddressController.java
@@ -1,5 +1,6 @@
 package vn.edu.iuh.fit.user.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,32 +8,24 @@ import org.springframework.web.bind.annotation.*;
 import vn.edu.iuh.fit.user.exceptions.UserException;
 import vn.edu.iuh.fit.user.model.dto.response.BaseResponse;
 import vn.edu.iuh.fit.user.model.dto.response.AddressResponse;
+import vn.edu.iuh.fit.user.services.AddressService;
 import vn.edu.iuh.fit.user.services.UserService;
 import vn.edu.iuh.fit.user.model.dto.request.AddressRequest;
-package vn.edu.iuh.fit.user.controllers;
-
-import io.lettuce.core.dynamic.annotation.Param;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import vn.edu.iuh.fit.user.model.dto.response.AddressResponse;
-import vn.edu.iuh.fit.user.model.dto.response.BaseResponse;
-import vn.edu.iuh.fit.user.services.AddressService;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import java.util.List;
 
 @RequestMapping("/api/v1/address")
 @RestController
+@Slf4j
 public class AddressController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AddressService addressService;
     //tìm địa chỉ user
     @GetMapping("/get-by-user-id/{id}")
     public ResponseEntity<BaseResponse<List<AddressResponse>>> getUserAddresses(@PathVariable("id") Long id) {
