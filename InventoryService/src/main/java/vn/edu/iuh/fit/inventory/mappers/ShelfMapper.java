316d06f6ee5adfa51930817cca433b89dd9bd8c8
Kiệt Trần <trantuankiet5980@gmail.com>
2025-03-06 14:14:19 +0700

    delete, save, find by localtion

--- InventoryService/src/main/java/vn/edu/iuh/fit/inventory/mappers/ShelfMapper.java
+++ InventoryService/src/main/java/vn/edu/iuh/fit/inventory/mappers/ShelfMapper.java
@@ -1,16 +1,17 @@
 package vn.edu.iuh.fit.inventory.mappers;
 
 import org.mapstruct.*;
+import vn.edu.iuh.fit.inventory.models.dtos.requests.ShelfRequest;
 import vn.edu.iuh.fit.inventory.models.dtos.responses.ShelfResponse;
 import vn.edu.iuh.fit.inventory.models.entities.Shelf;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ShelfMapper {
-    Shelf toEntity(Shelf shelf);
+    Shelf toEntity(ShelfRequest request);
 
     @Mapping(target = "inventory.id", source = "inventory")
     ShelfResponse toDto(Shelf shelf);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Shelf partialUpdate(Shelf shelf);
+    Shelf partialUpdate(ShelfRequest shelfRequest, @MappingTarget Shelf shelf);
 }
