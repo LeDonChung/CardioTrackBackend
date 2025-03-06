26759983706d634f7d61912a9355f09a38de394e
Kiệt Trần <trantuankiet5980@gmail.com>
2025-03-06 14:28:01 +0700

    add shelf

--- InventoryService/src/main/java/vn/edu/iuh/fit/inventory/repositories/ShelfRepository.java
+++ InventoryService/src/main/java/vn/edu/iuh/fit/inventory/repositories/ShelfRepository.java
@@ -1,6 +1,6 @@
 package vn.edu.iuh.fit.inventory.repositories;
 
-import org.bouncycastle.LICENSE;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import vn.edu.iuh.fit.inventory.models.entities.Shelf;
@@ -8,6 +8,6 @@ import vn.edu.iuh.fit.inventory.models.entities.Shelf;
 import java.util.List;
 
 public interface ShelfRepository extends JpaRepository<Shelf, Long> {
-    @Query("SELECT s FROM Shelf s WHERE s.location like %?1%")
-    List<Shelf> findShelfByLocation(String location);
+    @Query("SELECT s FROM Shelf s WHERE s.status = ?1")
+    List<Shelf> findShelfByStatus(String status, Pageable pageable);
 }
