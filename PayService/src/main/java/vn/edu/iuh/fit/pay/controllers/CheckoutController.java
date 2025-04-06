de17a154fdb19b6e1d33132c22587ce3ac34b2d6
vuphongle <lephong29092909@gmail.com>
2025-04-06 16:20:53 +0700

    Todo logic: pay-retry

--- PayService/src/main/java/vn/edu/iuh/fit/pay/controllers/CheckoutController.java
+++ PayService/src/main/java/vn/edu/iuh/fit/pay/controllers/CheckoutController.java
@@ -1,22 +1,18 @@
 package vn.edu.iuh.fit.pay.controllers;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.iuh.fit.pay.dto.PaymentRequest;
 import vn.edu.iuh.fit.pay.dto.ProductRequest;
 import vn.payos.PayOS;
+import vn.payos.exception.PayOSException;
 import vn.payos.type.CheckoutResponseData;
 import vn.payos.type.ItemData;
 import vn.payos.type.PaymentData;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -43,10 +39,6 @@ public class CheckoutController {
         final int price = request.getAmount();
         final Long orderCode = request.getOrderCode();
 
-
-        System.out.println("------------------------------------");
-        System.out.println("Request: " + request);
-
         List<ItemData> items = new ArrayList<>();
         for (ProductRequest product : products) {
             ItemData item = ItemData.builder()
@@ -66,12 +58,17 @@ public class CheckoutController {
                 .items(items)
                 .build();
 
-        System.out.println("Payment Data: " + paymentData);
-
         // Gọi PayOS để tạo payment link (nếu exception xảy ra, Retry sẽ kích hoạt)
-        CheckoutResponseData data = payOS.createPaymentLink(paymentData);
+        try {
+            CheckoutResponseData data = payOS.createPaymentLink(paymentData);
+            return ResponseEntity.ok(Map.of("checkoutUrl", data.getCheckoutUrl()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error creating payment link", e);
+        }
 
-        return ResponseEntity.ok(Map.of("checkoutUrl", data.getCheckoutUrl()));
+
+//        return ResponseEntity.ok(Map.of("checkoutUrl", data.getCheckoutUrl()));
     }
 
     public ResponseEntity<?> createPaymentLinkFallback(PaymentRequest request, Throwable throwable) {
