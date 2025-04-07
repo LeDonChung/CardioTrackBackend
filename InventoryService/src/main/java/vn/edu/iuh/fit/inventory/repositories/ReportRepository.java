0c4efbdce1b2be33c3109bc5e9a4c78cf7f784ac
Mai Chien No <nomai6789@gmail.com>
2025-04-07 17:58:28 +0700

    report inventory import by week, month, year

---
+++ InventoryService/src/main/java/vn/edu/iuh/fit/inventory/repositories/ReportRepository.java
@@ -0,0 +1,21 @@
+package vn.edu.iuh.fit.inventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import vn.edu.iuh.fit.inventory.models.entities.InventoryImport;
+
+@Repository
+public interface ReportRepository extends JpaRepository<InventoryImport, Long> {
+    //Thống kê lượng hàng nhập theo tuần
+    @Query(value = "SELECT COUNT(*) FROM inventory_import WHERE WEEK(import_date) = WEEK(CURRENT_DATE)", nativeQuery = true)
+    int countInventoryImportThisWeek();
+
+    //Thống kê lượng hàng nhập theo tháng
+    @Query(value = "SELECT COUNT(*) FROM inventory_import WHERE MONTH(import_date) = MONTH(CURRENT_DATE)", nativeQuery = true)
+    int countInventoryImportThisMonth();
+
+    //Thống kê lượng hàng nhập theo năm
+    @Query(value = "SELECT COUNT(*) FROM inventory_import WHERE YEAR(import_date) = YEAR(CURRENT_DATE)", nativeQuery = true)
+    int countInventoryImportThisYear();
+}
