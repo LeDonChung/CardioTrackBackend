e71c0ee768f2b4a6c4daef03f295aee7cdb0cc01
leDonChung <ledonchung12a2@gmail.com>
2025-05-22 18:30:32 +0700

    config terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,10 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  
 }
 
 resource "aws_s3_bucket" "trongtiniuh-bucket" {
   bucket = "trongtiniuh-bucket"
 }
 
-terraform {
-  backend "local" {
-    path = "/home/terraform.tfstate"
-  }
-}
